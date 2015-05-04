d492c57dbc0c4e3d27053474db55f1636f249ed3
Kaio Magalhães <kaio.zeroglosa@gmail.com>
2015-05-04 17:17:26 -0300

    init gem project

---
+++ lib/price/index/calculator.rb
@@ -0,0 +1,9 @@
+require "price/index/calculator/version"
+
+module Price
+  module Index
+    module Calculator
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/price/index/calculator/version.rb
@@ -0,0 +1,7 @@
+module Price
+  module Index
+    module Calculator
+      VERSION = "0.1.0"
+    end
+  end
+end
