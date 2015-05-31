fa15be4819518019b6ca4c78a25fef1db533583d
kaiomagalhaes <contato@kaiomagalhaes.com.br>
2015-05-31 15:26:27 -0300

    refactoring tests

--- spec/price/index/calculator_spec.rb
+++ spec/price/index/calculator_spec.rb
@@ -13,11 +13,16 @@ describe Price::Index::Calculator do
   end
 
   describe "test price_index_list" do
-    it 'price_index_list with empty list return empty list' do
+    it 'With empty list return empty list' do
       expect(calculator.price_index_list []).to eq([])
     end
 
-    it 'price_index_list with nil list return empty list' do
+    it 'With nil list return empty list' do
+      expect(calculator.price_index_list nil).to eq([])
+    end
+
+    it 'With valid list return correct list of products with index' do
+     skip 
       expect(calculator.price_index_list nil).to eq([])
     end
   end
@@ -33,45 +38,49 @@ describe Price::Index::Calculator do
     end
 
     it 'With old_product nil return new_product' do
-      new_product = {amount: 2, price: 2.8}
+      new_product = product 2, 2.0
       new_product_with_price_index = new_product.clone
       new_product_with_price_index[:price_index] = 100
       expect(calculator.price_index new_product , nil).to eq(new_product_with_price_index)
     end
 
     it 'With valid products return correct value' do
-      new_product = {amount: 2, price: 3.0}
-      old_product = {amount: 2, price: 2.0}
+      new_product = product 2, 3.0
+      old_product = product 2, 2.0
       new_product_with_price_index = new_product.clone
       new_product_with_price_index[:price_index] = 150
       expect(calculator.price_index new_product , old_product).to eq(new_product_with_price_index)
     end
 
     it 'With products with diferent amounts will use the amount of the old' do
-      new_product = {amount: 10, price: 3.0}
-      old_product = {amount: 2, price: 2.0}
+      new_product = product 10, 3.0
+      old_product = product 2 , 2.0
       new_product_with_price_index = new_product.clone
       new_product_with_price_index[:price_index] = 150
       expect(calculator.price_index new_product , old_product).to eq(new_product_with_price_index)
     end
 
     it 'With products with no value for :price will raise an exception' do
-      valid_product = {amount: 10, price: 20}
-      invalid_product = {amount: 2,}
+      valid_product = product 10,20
+      invalid_product = product 2, nil
 
       expect{calculator.price_index invalid_product, valid_product}.to raise_error(ArgumentError)
       expect{calculator.price_index valid_product,invalid_product }.to raise_error(ArgumentError)
     end
 
     it 'With products with no value for :amount will raise an exception' do
-      valid_product = {amount: 10, price: 20}
-      invalid_product = {price: 5.0,}
+      valid_product = product 10,20
+      invalid_product = product nil, 20
 
       expect{calculator.price_index invalid_product, valid_product}.to raise_error(ArgumentError)
       expect{calculator.price_index valid_product,invalid_product }.to raise_error(ArgumentError)
     end
-
-
-
   end
+
+  private
+
+  def product(amount,price, *price_index)
+    {amount: amount, price: price, price_index: price_index}
+  end
+
 end
