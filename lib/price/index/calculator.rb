62fadddd6d58719de457ecf43e436ebd8d298ebd
kaiomagalhaes <contato@kaiomagalhaes.com.br>
2015-05-30 21:21:30 -0300

    creating implementation for price_index with tests passing

--- lib/price/index/calculator.rb
+++ lib/price/index/calculator.rb
@@ -3,7 +3,23 @@ require "price/index/calculator/version"
 module Price
   module Index
     module Calculator
-      # Your code goes here...
+
+      def price_index(new_product, old_product)
+        return Hash.new if new_product.nil?
+
+        if old_product.nil?
+          new_product[:price_index] = 100
+          return new_product
+        end
+       
+        index = (new_product[:amount] * new_product[:price]) / (old_product[:amount] * old_product[:price])
+        new_product[:price_index] = index * 100
+        new_product
+      end
+
+      def price_index_list(products)
+       return [] if products.nil? || products.empty?
+      end
     end
   end
 end
--- spec/price/index/calculator_spec.rb
+++ spec/price/index/calculator_spec.rb
@@ -1,11 +1,51 @@
 require 'spec_helper'
 
 describe Price::Index::Calculator do
+
+  let(:calculator) do 
+      obj = Object.new
+      obj.extend Price::Index::Calculator
+      obj
+  end
+
   it 'has a version number' do
     expect(Price::Index::Calculator::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe "test price_index_list" do
+    it 'price_index_list with empty list return empty list' do
+      expect(calculator.price_index_list []).to eq([])
+    end
+
+    it 'price_index_list with nil list return empty list' do
+      expect(calculator.price_index_list nil).to eq([])
+    end
+  end
+
+  describe "test price_index"do
+
+    it 'With new_product nil return empty hash' do
+      expect(calculator.price_index nil, nil).to eq(Hash.new)
+    end
+
+    it 'With new_product nil return empty hash' do
+      expect(calculator.price_index nil, nil).to eq(Hash.new)
+    end
+
+    it 'With old_product nil return new_product' do
+      new_product = {amount: 2, price: 2.8}
+      new_product_with_price_index = new_product.clone
+      new_product_with_price_index[:price_index] = 100
+      expect(calculator.price_index new_product , nil).to eq(new_product_with_price_index)
+    end
+
+     it 'With valid products return correct value' do
+      new_product = {amount: 2, price: 3.0}
+      old_product = {amount: 2, price: 2.0}
+      new_product_with_price_index = new_product.clone
+      new_product_with_price_index[:price_index] = 150
+      expect(calculator.price_index new_product , old_product).to eq(new_product_with_price_index)
+    end
+
   end
 end
